46ce92061a21d84a9d26775e9c913a254c779f00
NadjlaSefadine <alzantoutienadjla@gmail.com>
2025-06-18 09:45:56 +0100

    finish

--- script.js
+++ script.js
@@ -3,36 +3,64 @@ let tasks = document.getElementById("tasks");
 //console.log(tasks);
 
 const addTask = () => {
+
     let task = inputField.value.trim();
     const li = document.createElement("li");
-li.innerHTML = `
+    li.innerHTML = `
 <label>
         <input type="checkbox">
-        <span>Terminer mon travail<</span>
+        <span>${task}</span>
     </label>
     <span class="editBtn"><i class="fa-solid fa-pen-to-square"></i></span>
     <span class="deletBtn"><i class="fa-solid fa-trash"></i></span>
 </li>
 `
-tasks.appendChild(li);
-inputField.value = "";
-//console.log(li);
+    tasks.appendChild(li);
+    inputField.value = "";
+    //console.log(li);
 
-const checkbox = li.querySelector("input");
-let span1 = li.querySelector("span");
-const editBtn = li.querySelector(".editBtn");
-//console.log(checkbox);
-checkbox.addEventListener("Click",() => {
-    span1.classList.toggle("checked");
-    console.log(span1.classList);
-});
+    const checkbox = li.querySelector("input");
+    let span1 = li.querySelector("span");
+    const editBtn = li.querySelector(".editBtn");
+    const deleteBtn = li.querySelector(".deletBtn");
 
-editBtn.addEventListener("click",function(){;
-   const taskupdate = prompt("editer tache:",span1.textContent);
-   if (taskupddate !== null){
-  span1.textcontent = taskupdate;
-  checkbox.checked = false;
-  span1.classList.remove("checked");
+    console.log(deleteBtn);
+    checkbox.addEventListener("click", () => {
+        span1.classList.toggle("checked");
+        console.log(span1.classList);
+        counter();
+    });
+
+    editBtn.addEventListener("click", function () {
+        console.log("hello");
+
+        const taskupdate = prompt("editer tache:", span1.textContent);
+        if (taskupdate !== null) {
+            span1.textContent = taskupdate;
+            checkbox.checked = false;
+            span1.classList.remove("checked");
+        }
+        counter();
+    });
+
+
+    deleteBtn.addEventListener("click", function () {
+        if (confirm("Êtes-vous sûr de vouloir tout effacer?")) {
+            li.remove();
+        }
+    });
+
+
+    // Exemple de taskUpdate : toggle terminé ou non
+    // if (e.target.type === "checkbox") {
+    //     updateCounters(); // ou taskUpdate(e.target) si tu l'as
+    // }
 }
-})
-};
+
+
+const counter = () => {
+    let completedTasks = document.querySelectorAll(".checked").length;
+    let compteur = document.getElementById("CompletedCounter");
+    compteur.innerText = completedTasks;
+}
+counter();
